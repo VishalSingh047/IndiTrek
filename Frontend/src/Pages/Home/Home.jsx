34e3827c1994ed8b08af4e1b887c7b0a1b6d07bf
Vishal Singh <vs997131@gmail.com>
2025-06-03 20:52:11 +0530

    Update

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home () {
 return<>
-<h1>Looking for some content</h1>
+<h1>Looking for some content to fill the Home Section</h1>
 <p>Have a great day</p>
 </>
 }
